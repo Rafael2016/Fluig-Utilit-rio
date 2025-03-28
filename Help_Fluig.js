864946a3d10053cac18dc88451f907f6f52282d0
Rafael Luz <rafa.tecnologo@hotmail.com>
2025-03-28 12:02:23 -0300

    Feat      Lista Atividade Processo

--- Help_Fluig.js
+++ Help_Fluig.js
@@ -28,3 +28,34 @@ var result = workflowEngine.cancelInstance(
                                            "Nº Solicitação", 
                                            "Matrícula usuário", // Tem que ser o Gestor ou usuário que abriu a solicitação
                                            "Mensagem")
+
+
+/***
+ * @LISTA TODA ATIVIDADE DE PROCESSO
+ *
+ */
+let fields = ["processStatePK.processId", "stateName", "stateDescription", "processStatePK.sequence"];
+let orders = ["processStatePK.sequence"];
+let  param = [
+    DatasetFactory.createConstraint("processStatePK.processId", "movimentacao_funcional", "movimentacao_funcional", ConstraintType.MUST)
+]
+let ds     = DatasetFactory.getDataset("processState", fields, param, orders);
+let vals  = ds.values;
+
+// Criando um Set para armazenar itens únicos
+let uniqueItems = new Set();
+
+// Ordenando os valores pelo atributo "processStatePK.processId"
+vals.sort((a, b) => a["processStatePK.processId"].localeCompare(b["processStatePK.processId"]));
+
+for (let i = 0; i < vals.length; i++) {
+    let item = vals[i];
+    let key = `${item["processStatePK.processId"]},${item["stateDescription"]},${item["stateName"]},${item["processStatePK.sequence"]}`;
+    
+    // Adicionando ao Set para evitar duplicatas
+    if (!uniqueItems.has(key)) {
+        ;
+        uniqueItems.add(key);
+    }
+}
+                                            
